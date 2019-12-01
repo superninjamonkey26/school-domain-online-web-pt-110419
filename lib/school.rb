208644d3b436b1adf3e8b2821f53f9ff963384c3
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-12-01 01:34:27 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
-# code here!
+class School
+  attr_reader :school_name, :roster
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+  def grade(grade)
+    @roster[grade]
+  end
+  def sort
+    @roster.each do |key, value|
+      @roster[key]=value.sort
+    end
+  end
+end
